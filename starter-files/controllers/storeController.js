329ef0f328115b71b5559ea7faecc4bbbb7b09b4
Michael Friedman <contact@michaeldavidfriedman.com>
2017-05-21 15:52:24 -0700

    database storage setup. starting control flow

--- starter-files/controllers/storeController.js
+++ starter-files/controllers/storeController.js
@@ -6,3 +6,7 @@ exports.homePage = (req, res) => {
 exports.addStore = (req, res) => {
   res.render('editStore', { title: '💩 Add Store' });
 };
+
+exports.createStore = (req, res) => {
+  res.json(req.body);
+};
