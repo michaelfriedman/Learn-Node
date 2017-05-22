ca0e000323718d752b69bfc3c7f76ebf4e50898d
Michael Friedman <contact@michaeldavidfriedman.com>
2017-05-22 06:16:21 -0700

    Geolocation services are setup and functioning properly using the Google Places API

---
+++ starter-files/public/javascripts/modules/autocomplete.js
@@ -0,0 +1,11 @@
+export default (input, latInput, lngInput) => {
+  if (!input) return; // skip this fn from running if there is not input on the page
+  const dropdown = new google.maps.places.Autocomplete(input);
+  dropdown.addListener('place_changed', () => {
+    const place = dropdown.getPlace();
+    latInput.value = place.geometry.location.lat();
+    lngInput.value = place.geometry.location.lng();
+  });
+  // If somone hits enter on the address field, don't subit the form
+  input.on('keydown', event => event.keyCode === 13 ? event.preventDefault() : null);
+};
