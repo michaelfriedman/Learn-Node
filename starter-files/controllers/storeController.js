6225a0da1633ffd6cd8759444ddd8d02f7629fcb
Michael Friedman <contact@michaeldavidfriedman.com>
2017-05-22 03:25:17 -0700

    Creating an editing flow for stores

--- starter-files/controllers/storeController.js
+++ starter-files/controllers/storeController.js
@@ -1,5 +1,8 @@
+const mongoose = require('mongoose');
+const Store = mongoose.model('Store');
+
 exports.homePage = (req, res) => {
-  console.log(req.name);
+  // console.log(req.name);
   res.render('index');
 };
 
@@ -7,6 +10,14 @@ exports.addStore = (req, res) => {
   res.render('editStore', { title: '💩 Add Store' });
 };
 
-exports.createStore = (req, res) => {
-  res.json(req.body);
+exports.createStore = async (req, res) => {
+  const store = await (new Store(req.body)).save();
+  req.flash('success', `Successfully Created ${store.name}. Care to leave a review?`);
+  res.redirect(`/store/${store.slug}`);
+};
+
+exports.getStores = async (req, res) => {
+  // 1. Query the database for a list of all the stores
+  const stores = await Store.find();
+  res.render('stores', { title: 'Stores', stores });
 };
