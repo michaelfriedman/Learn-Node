e3c9dc863b223234ad9470ca4157e9ec891e098b
Michael Friedman <contact@michaeldavidfriedman.com>
2017-05-22 08:52:00 -0700

    Uploading and resizing images with middleware

--- starter-files/controllers/storeController.js
+++ starter-files/controllers/storeController.js
@@ -1,13 +1,45 @@
 const mongoose = require('mongoose');
 const Store = mongoose.model('Store');
+const multer = require('multer');
+const jimp = require('jimp');
+const uuid = require('uuid');
+
+const multerOptions = {
+  storage: multer.memoryStorage(),
+  fileFilter (req, file, next) {
+    const isPhoto = file.mimetype.startsWith('image/');
+    if (isPhoto) {
+      next(null, true);
+    } else {
+      next({ message: 'That file type isn\t allowed!' }, false);
+    }
+  }
+};
 
 exports.homePage = (req, res) => {
-  // console.log(req.name);
   res.render('index');
 };
 
 exports.addStore = (req, res) => {
   res.render('editStore', { title: '💩 Add Store' });
+};
+
+exports.upload = multer(multerOptions).single('photo');
+
+exports.resize = async (req, res, next) => {
+  // check if there is no new file to resize
+  if (!req.file) {
+    next(); // skip to the next middleware
+    return;
+  }
+  const extension = req.file.mimetype.split('/')[1];
+  req.body.photo = `${uuid.v4()}.${extension}`;
+  // Now we resize
+  const photo = await jimp.read(req.file.buffer);
+  await photo.resize(800, jimp.AUTO);
+  await photo.write(`./public/uploads/${req.body.photo}`);
+  // once we have written the photo to our filesystem, keep going!
+  next();
 };
 
 exports.createStore = async (req, res) => {
