436d32b30efeb9efc7c072f6874861f134261b11
Michael Friedman <contact@michaeldavidfriedman.com>
2017-05-23 09:24:21 -0700

    custom queries and hooks setup

--- starter-files/models/Store.js
+++ starter-files/models/Store.js
@@ -50,7 +50,15 @@ storeSchema.pre('save', async function (next) {
     this.slug = `${this.slug}-${storesWithSlug.length + 1}`;
   }
   next();
-  // TODO: make more resiliant so lugs are unique
+  // TODO: make more resiliant so slugs are unique
 });
+
+storeSchema.statics.getTagsList = function () {
+  return this.aggregate([
+    { $unwind: '$tags' },
+    { $group: { _id: '$tags', count: { $sum: 1 } } },
+    { $sort: { count: -1 } }
+  ]);
+};
 
 module.exports = mongoose.model('Store', storeSchema);
