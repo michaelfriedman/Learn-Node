db0e16e98a9d069390a336d0f492d85ebab85d8c
Michael Friedman <contact@michaeldavidfriedman.com>
2017-05-25 04:49:50 -0700

    user authentication working successfully using Passport.js

--- starter-files/routes/index.js
+++ starter-files/routes/index.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const storeController = require('../controllers/storeController');
 const userController = require('../controllers/userController');
 const { catchErrors } = require('../handlers/errorHandlers');
-
+const authController = require('../controllers/authController');
 // Do work here
 router.get('/', catchErrors(storeController.getStores));
 router.get('/stores', catchErrors(storeController.getStores));
@@ -29,5 +29,12 @@ router.get('/tags', catchErrors(storeController.getStoresByTag));
 router.get('/tags/:tag', catchErrors(storeController.getStoresByTag));
 
 router.get('/login', userController.loginForm);
+
+router.get('/register', userController.registerForm);
+router.post('/register',
+userController.validateRegister,
+userController.register,
+authController.login
+);
 
 module.exports = router;
